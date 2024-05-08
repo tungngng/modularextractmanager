0f53f5bfbf4d1cb8f956f0d93e8111d6155e3011
tungngng <mangato244pun0suy@hotmail.com>
2024-05-08 11:45:26 +0700

    aptos

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const maxN = (arr, n = 1) => [...arr].sort((a, b) => b - a).slice(0, n);
+function numDecodings(s) {
+  const dp = new Array(s.length + 1).fill(0);
+  dp[0] = 1;
+  dp[1] = s[0] === "0" ? 0 : 1;
+  for (let i = 2; i <= s.length; i++) {
+    const oneDigit = parseInt(s.substring(i - 1, i));
+    const twoDigits = parseInt(s.substring(i - 2, i));
+    if (oneDigit >= 1) {
+      dp[i] += dp[i - 1];
+    }
+    if (twoDigits >= 10 && twoDigits <= 26) {
+      dp[i] += dp[i - 2];
+    }
+  }
+  return dp[s.length];
+}
